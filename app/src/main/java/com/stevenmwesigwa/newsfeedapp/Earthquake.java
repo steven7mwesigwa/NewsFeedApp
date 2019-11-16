3784683920ac159d8479bc896fa11d1d9b42df60
Steven Mwesigwa <contact@stevenmwesigwa.com>
2019-11-16 21:08:10 +0300

    Initial Commit

---
+++ app/src/main/java/com/stevenmwesigwa/newsfeedapp/Earthquake.java
@@ -0,0 +1,61 @@
+package com.stevenmwesigwa.newsfeedapp;
+/**
+ * An {@link Earthquake} object contains information related to a single earthquake.
+ */
+public class Earthquake {
+
+    /**
+     * Magnitude of the earthquake
+     */
+    private double mMagnitude;
+
+    /**
+     * Location of the earthquake
+     */
+    private String mLocation;
+
+    /** Date of the earthquake */
+    private String mDate;
+
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
+    private String mUrl;
+
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
